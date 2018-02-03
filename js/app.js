c2759335b455b32b40f874fd6cf613b2f68e33ba
Simon Morley <simon@polkaspots.com>
2018-02-03 19:46:42 +0000

    Meg

--- js/app.js
+++ js/app.js
@@ -20,4 +20,25 @@ function closeNav() {
     document.getElementById("navTray").style.height = "0%";
 }
 
-console.log("%cWelcome To CT WiFi", "font-size: x-large")
+console.log("%cWelcome to the jungle, what's going on pal?", "font-size: x-large")
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+$('input#submitButton').click( function() {
+  var email = $('input[name="email"]').val();
+  if (email && isEmail(email)) {
+    $.ajax({
+      url: 'http://app.mimo.test:8080/api/v1/holding_accounts',
+      type: 'post',
+      dataType: 'json',
+      data: { email: email },
+      success: function(data) {
+        $('.signup').hide();
+        $('.signedup').show();
+      }
+    });
+  }
+});
