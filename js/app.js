5540414089ccc068fe3b3fb78082f6219dfcf033
Simon Morley <simon@polkaspots.com>
2018-02-19 19:04:28 +0000

    FB Msg

--- js/app.js
+++ js/app.js
@@ -150,7 +150,6 @@ $('.engage').on('click', function() {
 });
 
 // drift.on('ready',function(api){
-//   $('.contact').click( function() {
-//     api.goToNewConversation();
-//   });
+$('.contact').click( function() {
+});
 // });
